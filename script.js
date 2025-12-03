a119393dc9a3764c5acdce331cb09ffd7bedf65e
Ajbajibanpm <elenadivino@gmail.com>
2025-12-03 14:28:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+// operazioni di preparazione
+// Recupero gli elementi di interesse della pagina:
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const chatBox = document.querySelector('.chat-box')
+
+// preparazione dei messaggi iniziali
+const messages = [
+    {
+    type: 'sent',
+    text: 'Ciao, come va?',
+    time: '03/12/2025 12:05:00'
+},
+{
+    type: 'received',
+    text: 'Benissimo, grazie. E tu?',
+    time: '03/12/2025 12:05:00'
+}
+]
+
+//# OPERAZIONI DI AVVIO PAGINA
+
+ //Svuoto la chat-box
+    chatBox.innerHTML = '';
+    //per ciascuno dei messaggi..
+for (const message of messages) {
+    chatBox.innerHTML += `
+<div class="chat-row ${message.type}">
+    <div class="chat-message">
+        <p>${message.text}</p>
+        <time datetime="${message.time}">${message.time}</time>
+    </div>
+</div>`
+}
+
+//# OPERAZIONI DI INTERAZIONE CON L'UTENTE
+// Al click del bottone..
+button.addEventListener('click',function(){
+    const insertedText = input.value.trim(); //"trim" toglie gli spazi superflui
+
+    if(insertedText === '') return //se non c'è testo l'invio viene annullato
+
+    const newMessage = {
+        type: 'sent',
+        text: insertedText,
+        time: new Date().toLocaleString()
+    }
+
+ messages.push(newMessage)
+
+ //Svuoto la chat-box
+    chatBox.innerHTML = '';
+
+ //per ciascuno dei messaggi..
+for (const message of messages) {
+    chatBox.innerHTML += `
+<div class="chat-row ${message.type}">
+    <div class="chat-message">
+        <p>${message.text}</p>
+        <time datetime="${message.time}">${message.time}</time>
+    </div>
+</div>`
+}
+input.value = ''
+input.focus()
+})
